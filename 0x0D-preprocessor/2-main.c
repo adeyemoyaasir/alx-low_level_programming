50ed89474c5bca4b1dad364799bae4238dc6ed36
adeyemoyaasir <adeyemoyaasir@gmail.com>
2022-10-10 13:16:03 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main-prints the name of the file of the program was compiled from, followed by a new line
+ * Return :Always 0
+ */
+int main (void)
+{
+   printf ("%s\n",_FILE_);
+   return (0);
+}	
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < o? (-x) : (x))
+
+#endif
